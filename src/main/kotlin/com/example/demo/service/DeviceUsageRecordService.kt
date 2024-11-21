49e1276e104048e048ededd4ea043b656862a4fe
TroyKang <kdu3840@gmail.com>
2024-11-21 22:05:13 +0900

    feat : Connect DeviceUsageRecord with Sendbird

---
+++ src/main/kotlin/com/example/demo/service/DeviceUsageRecordService.kt
@@ -0,0 +1,40 @@
+package com.example.demo.service
+
+import com.example.demo.dto.DeviceUsageCreateDTO
+import com.example.demo.model.DeviceUsageRecord
+import com.example.demo.model.UserDevice
+import com.example.demo.repository.DeviceUsageRecordRepository
+import kotlin.random.Random
+import com.example.demo.repository.UserDeviceRepository
+import org.springframework.stereotype.Service
+import java.time.LocalDateTime
+import java.util.*
+
+@Service
+class DeviceUsageRecordService(
+    private val deviceUsageRecordRepository: DeviceUsageRecordRepository,
+    private val userDeviceRepository: UserDeviceRepository
+) {
+
+    fun addDeviceUsageRecord(request: DeviceUsageCreateDTO) {
+        val userDevice = userDeviceRepository.findUserDeviceByUserIdAndDeviceId(
+            UUID.fromString(request.userId),
+            UUID.fromString(request.deviceId)
+        )
+        
+        val now = LocalDateTime.now()
+        val randomMinutes = Random.nextInt(1, 60)
+        val startTime = now.minusMinutes(randomMinutes.toLong())
+
+        val record = DeviceUsageRecord(
+            id = UUID.randomUUID(),
+            userDevice = userDevice,
+            usageDate = now.toLocalDate(),
+            actionDescription = request.actionDescription,
+            startTime = startTime,
+            endTime = now
+        )
+
+        deviceUsageRecordRepository.save(record)
+    }
+}
